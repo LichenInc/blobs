8fff7a5afe9fe4016d6f969c1d0cc006913f862c
gabriel <gcorbel@lichencommunications.com>
2020-01-23 15:04:08 -0500

    add idle

--- src/components/default.vue
+++ src/components/default.vue
@@ -22,7 +22,7 @@
     div(id='blob')
     div current path: {{currentPath}}
     div old path: {{oldPath}}
-    button(@click='startIdle = !startIdle') Idle animation
+    button(@click='goIdle') Idle animation
 </template>
 
 <script>
@@ -39,7 +39,8 @@ export default {
     complexite: { required: false, type: Number, default: 0 },
     couleur_debut: { required: false, type: String, default: '' },
     couleur_fin: { required: false, type: String, default: '' },
-    transition: { required: false, type: Number, default: 0 }
+    transition: { required: false, type: Number, default: 0 },
+    effects: { required: false, type: Array, default: () => [] }
   },
   data () {
     return {
@@ -50,6 +51,7 @@ export default {
       oldPath: null,
       angleFactor: 360,
       startIdle: false,
+      stockPoint: null,
       blobOptions: {        
         closed: true,
         fill: '#ec576b',
@@ -140,6 +142,7 @@ export default {
     },
     'couleur_debut' (newVal) {
       if (newVal) {
+        this.blobOptions.fill = newVal
         let svg = document.getElementById('svblob')
         if (svg.children[0]) {
           const g = svg.children[0]
@@ -211,6 +214,91 @@ export default {
     this.renderBlob()
   },
   methods: {
+    goIdle () {
+      this.startIdle = !this.startIdle
+      if (this.startIdle) {
+        let up = true
+        window.interAnim = setInterval(() => {
+          if (up) {
+            up = false
+          } else {
+            up = true
+          }
+          console.log(up)
+          this.changValue(up)
+        }, 500)
+      } else {
+        clearInterval(window.interAnim)
+      }
+    },
+    changValue(up) {
+      const count = 3 + Math.floor(this.countFactor * 0.4)
+      const angle = 360 / count
+      const tempTab = []
+      this.oldPath = this.currentPath
+      for (let i = 0; i < this.stockPoint.length; i++) {
+        if (up) {
+          tempTab.push({ x: this.stockPoint[i].x + i + this.rgen(), y: this.stockPoint[i].y + i + this.rgen() })
+        } else {
+          tempTab.push({ x: this.stockPoint[i].x - i  - this.rgen(), y: this.stockPoint[i].y - i - this.rgen() })
+        }
+      }
+      const smoth = smooth(tempTab, {
+        closed: true,
+        strength: ((4 / 3) * Math.tan(rad(angle / 4))) / Math.sin(rad(angle / 2)) + Math.random(0,1), // le math random rend ça plus carré si on le + il est plus rond
+      })
+      let temp = renderEditable(smoth, this.blobOptions)
+      const enf = temp.children[0].children[0]
+      this.currentPath = enf.attributes.d
+      let svg = document.getElementById('svblob')
+      if (svg.children[0]) {
+        const g = svg.children[0]
+        const path = g.children[0]
+        this.$nextTick(() => {
+          path.setAttribute('fill', this.blobOptions.fill) // ON METS LA NOUVELLE COULEUR
+          path.setAttribute('d', this.currentPath) // ON METS LE NOUVEAU PATH ICI
+        })
+      }
+      // let temp = this.currentPath.split(',') // on split la chaine de charact;re sur la virgule
+      // for (let i = 1; i < temp.length; i++) { // On commence à 1 parce qu'on veut toujours dessiné le svg au même endroit
+      //   let val = Object.assign( {} ,temp[i])
+      //   let newVal = val
+      //   if (up) {
+      //     newVal = parseInt(newVal[4]) + 1
+      //     if (newVal < 10) {
+      //       val[4] = newVal.toString()
+      //       temp[i] = Object.values(val).join().replace(/,/g, '')
+      //     } else {
+      //       // on doit changer la valeur [1] et [2]
+      //       val[2] = '1'
+      //       val[4] = '0'
+      //       temp[i] = Object.values(val).join().replace(/,/g, '')
+      //     }
+      //   } else {
+      //     newVal = parseInt(newVal[4]) - 1
+      //     if (newVal > 0) {
+      //       val[4] = newVal.toString()
+      //       temp[i] = Object.values(val).join().replace(/,/g, '')
+      //     }
+      //   }
+      //   // console.log(temp)
+      //   // temp = temp.join()
+      //   let diff = Object.assign( {} ,temp)
+      //   console.log(diff)
+      //   diff = Object.values(diff)
+      //   this.oldPath = this.currentPath
+      //   this.currentPath = diff.join()
+      //   let svg = document.getElementById('svblob')
+      //   if (svg.children[0]) {
+      //     const g = svg.children[0]
+      //     const path = g.children[0]
+      //     this.$nextTick(() => {
+      //       path.setAttribute('fill', this.blobOptions.fill) // ON METS LA NOUVELLE COULEUR
+      //       path.setAttribute('d', diff) // ON METS LE NOUVEAU PATH ICI
+      //     })
+      //   }       
+      // }
+    },
     forceWaitin() {
       this.$nextTick(() => '')
     },
@@ -233,6 +321,7 @@ export default {
             y: Math.cos(rad(i * angle)) * radius * random + 600 / 2
         })
       }
+      this.stockPoint = points
       const smoth = smooth(points, {
         closed: true,
         strength: ((4 / 3) * Math.tan(rad(angle / 4))) / Math.sin(rad(angle / 2)),
@@ -255,7 +344,7 @@ export default {
         this.currentPath = path.getAttribute('d')
         // path.setAttribute('stroke', enf.attributes.fill)
         // path.setAttribute('stroke-width', 2)
-        console.log(this.blobOptions.fill)
+        // console.log(this.blobOptions.fill)
         path.setAttribute('fill', this.blobOptions.fill)
         g.appendChild(path)
         elem.appendChild(g)
@@ -274,6 +363,7 @@ export default {
             y: Math.cos(rad(i * angle)) * radius * random + 600 / 2
         })
       }
+      this.stockPoint = points
       const smoth = smooth(points, {
         closed: true,
         strength: ((4 / 3) * Math.tan(rad(angle / 4))) / Math.sin(rad(angle / 2)),
@@ -286,7 +376,10 @@ export default {
       if (svg.children[0]) {
         const g = svg.children[0]
         const path = g.children[0]
-        path.setAttribute('d', this.currentPath) // ON METS LE NOUVEAU PATH ICI
+        this.$nextTick(() => {
+          path.setAttribute('fill', this.blobOptions.fill) // ON METS LA NOUVELLE COULEUR
+          path.setAttribute('d', this.currentPath) // ON METS LE NOUVEAU PATH ICI
+        })
       }
     }
   }
