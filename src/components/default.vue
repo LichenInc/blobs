3a3cad69c1394f14349583bfe72dcd450acd841a
gabriel <gcorbel@lichencommunications.com>
2020-01-23 11:33:36 -0500

    add random slider and oldPath for greensock

--- src/components/default.vue
+++ src/components/default.vue
@@ -14,9 +14,15 @@
       span count countFactor
       input(type='range', min=1, max=100, step=1, v-model='countFactor')
     div
-      h3 RANDOM FACTOR
+      span RANDOM FACTOR
       input(type='range', min=1, max=100, step=1, v-model='randomFactor')
+    div
+      span Angle influencer (1 / 360)
+      input(type='range', min=1, max=100, step=1, v-model='angleFactor')
     div(id='blob')
+    div current path: {{currentPath}}
+    div old path: {{oldPath}}
+    button(@click='startIdle = !startIdle') Idle animation
 </template>
 
 <script>
@@ -40,6 +46,10 @@ export default {
       // blobOptions.complexity: 0.4,
       countFactor: 14,
       randomFactor: 20,
+      currentPath: null,
+      oldPath: null,
+      angleFactor: 360,
+      startIdle: false,
       blobOptions: {        
         closed: true,
         fill: '#ec576b',
@@ -61,73 +71,104 @@ export default {
   watch: {
     'countFactor' (newVal, oldVal) {
       if (newVal !== oldVal) {
-        const temp = document.getElementById('blob')
-        while(temp.firstChild) {
-          temp.removeChild(temp.firstChild)
+        // console.log(document.getElementById('svblob'))
+        let svg = document.getElementById('svblob')
+        if (svg.children[0]) {
+          // console.log(svg.children[0])
+          const g = svg.children[0]
+          const path = g.children[0]
+          // console.log('g', g)
+          this.oldPath = path.getAttribute('d')
         }
-        this.renderBlob()
+        this.renderNewValBlob()
       }
     },
     'blobOptions.complexity' (newVal, oldVal) {
       if (newVal !== oldVal) {
-        const temp = document.getElementById('blob')
-        while(temp.firstChild) {
-          temp.removeChild(temp.firstChild)
+        let svg = document.getElementById('svblob')
+        if (svg.children[0]) {
+          const g = svg.children[0]
+          const path = g.children[0]
+          this.oldPath = path.getAttribute('d')
         }
-        this.renderBlob()
+        this.renderNewValBlob()
       }
     },
     'blobOptions.seed' (newVal, oldVal) {
       if (newVal !== oldVal) {
-        const temp = document.getElementById('blob')
-        while(temp.firstChild) {
-          temp.removeChild(temp.firstChild)
+        let svg = document.getElementById('svblob')
+        if (svg.children[0]) {
+          const g = svg.children[0]
+          const path = g.children[0]
+          this.oldPath = path.getAttribute('d')
         }
-        this.renderBlob()
+        this.renderNewValBlob()
       }
     },
     'blobOptions.contrast' (newVal, oldVal) {
       if (newVal !== oldVal) {
-        const temp = document.getElementById('blob')
-        while(temp.firstChild) {
-          temp.removeChild(temp.firstChild)
+        let svg = document.getElementById('svblob')
+        if (svg.children[0]) {
+          const g = svg.children[0]
+          const path = g.children[0]
+          this.oldPath = path.getAttribute('d')
         }
-        this.renderBlob()
+        this.renderNewValBlob()
       }
     },
     'nb_points' (newVal) {
         if (newVal) {
-          this.blobOptions.complexity = newVal
-          const temp = document.getElementById('blob')
-          while(temp.firstChild) {
-            temp.removeChild(temp.firstChild)
+          let svg = document.getElementById('svblob')
+          if (svg.children[0]) {
+            const g = svg.children[0]
+            const path = g.children[0]
+            this.oldPath = path.getAttribute('d')
           }
-          this.renderBlob()
+          this.renderNewValBlob()
         }
     },
     complexite (newVal) {
       if (newVal) {
-        this.blobOptions.contrast = newVal
-        const temp = document.getElementById('blob')
-        while(temp.firstChild) {
-          temp.removeChild(temp.firstChild)
+        let svg = document.getElementById('svblob')
+        if (svg.children[0]) {
+          const g = svg.children[0]
+          const path = g.children[0]
+          this.oldPath = path.getAttribute('d')
         }
-        this.renderBlob()
+        this.renderNewValBlob()
       }
     },
     'couleur_debut' (newVal) {
       if (newVal) {
-        this.blobOptions.fill = newVal
-        const temp = document.getElementById('blob')
-        while(temp.firstChild) {
-          temp.removeChild(temp.firstChild)
+        let svg = document.getElementById('svblob')
+        if (svg.children[0]) {
+          const g = svg.children[0]
+          const path = g.children[0]
+          this.oldPath = path.getAttribute('d')
         }
-        this.renderBlob()
+        this.renderNewValBlob()
       }
     },
     'randomFactor' (newVal) {
       if (newVal) {
-        this.renderBlob()
+        let svg = document.getElementById('svblob')
+        if (svg.children[0]) {
+          const g = svg.children[0]
+          const path = g.children[0]
+          this.oldPath = path.getAttribute('d')
+        }
+        this.renderNewValBlob()
+      }
+    },
+    'angleFactor' (newVal) {
+      if (newVal) {
+        let svg = document.getElementById('svblob')
+        if (svg.children[0]) {
+          const g = svg.children[0]
+          const path = g.children[0]
+          this.oldPath = path.getAttribute('d')
+        }
+        this.renderNewValBlob()
       }
     }
   },
@@ -140,7 +181,7 @@ export default {
       return 600 / Math.E
     },
     angle () {
-      return (360 / this.count)
+      return (Math.random(1, this.angleFactor) / this.count)
       // return 20 * Math.PI / this.count
     },
     calcseed () {
@@ -200,7 +241,7 @@ export default {
       this.blobOptions['transform'] = `rotate(${this.rgen() * this.angle / Math.PI},${600 / 2},${600 / 2})`
       let temp = renderEditable(smoth, this.blobOptions)
       const elem = document.createElementNS('http://www.w3.org/2000/svg', temp.tag)
-      elem.id = 'svblob'
+      elem.id = 'svblob' 
       elem.setAttribute('width', 600)
       elem.setAttribute('height', 600)
       elem.setAttribute('viewbox', temp.attributes.viewBox)
@@ -211,12 +252,41 @@ export default {
         const enf = temp.children[i].children[0]
         const path = document.createElementNS('http://www.w3.org/2000/svg', enf.tag)
         path.setAttribute('d', enf.attributes.d)
+        this.currentPath = path.getAttribute('d')
         // path.setAttribute('stroke', enf.attributes.fill)
         // path.setAttribute('stroke-width', 2)
         console.log(this.blobOptions.fill)
         path.setAttribute('fill', this.blobOptions.fill)
         g.appendChild(path)
         elem.appendChild(g)
+      }
+    },
+    renderNewValBlob () {
+      const count = 3 + Math.floor(this.countFactor * 0.4)
+      const angle = 360 / count
+      // const angle = 90
+      const radius = 300 / Math.E
+      const points = []
+      for (let i = 0; i < count; i++) {
+        const random = 1 - 0.8 * this.blobOptions.contrast * this.rgen()
+        points.push({
+            x: Math.sin(rad(i * angle)) * radius * random + 600 / 2,
+            y: Math.cos(rad(i * angle)) * radius * random + 600 / 2
+        })
+      }
+      const smoth = smooth(points, {
+        closed: true,
+        strength: ((4 / 3) * Math.tan(rad(angle / 4))) / Math.sin(rad(angle / 2)),
+      })
+      this.blobOptions['transform'] = `rotate(${this.rgen() * this.angle / Math.PI},${600 / 2},${600 / 2})`
+      let temp = renderEditable(smoth, this.blobOptions)
+      const enf = temp.children[0].children[0]
+      this.currentPath = enf.attributes.d
+      let svg = document.getElementById('svblob')
+      if (svg.children[0]) {
+        const g = svg.children[0]
+        const path = g.children[0]
+        this.oldPath = path.setAttribute('d', this.currentPath)
       }
     }
   }
